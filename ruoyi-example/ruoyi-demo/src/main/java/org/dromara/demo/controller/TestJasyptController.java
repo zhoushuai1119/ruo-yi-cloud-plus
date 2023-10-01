fc0b3020f6ff8053977e0d6f326a02b38c994181
zhoushuai <zs11195310@163.com>
2023-10-01 20:36:17 +0800

    添加jasypt配置文件加密

---
+++ ruoyi-example/ruoyi-demo/src/main/java/org/dromara/demo/controller/TestJasyptController.java
@@ -0,0 +1,46 @@
+package org.dromara.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.dromara.common.core.domain.R;
+import org.jasypt.encryption.StringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * jasypt加密的样例
+ *
+ * @author Zhou Shuai
+ */
+@RestController
+@RequestMapping("/jasypt")
+@RequiredArgsConstructor
+public class TestJasyptController {
+
+    private final StringEncryptor stringEncryptor;
+
+    @Value("${jasypt.test.value}")
+    private String jasyptValue;
+
+    /**
+     * jasypt配置文件加解密测试
+     */
+    @GetMapping("/test")
+    public R<String> jasyptTest(@RequestParam("value") String value) {
+        // 生产环境建议使用命令行参数设置password
+        System.setProperty("jasypt.encryptor.password", "EbfYkitulv73I2p0mXI50JMXoaxZTKJ7");
+        String encValue = stringEncryptor.encrypt(value);
+        return R.ok(encValue);
+    }
+
+    /**
+     * jasypt解密测试
+     */
+    @GetMapping("/value")
+    public R<String> jasyptValue() {
+        return R.ok(jasyptValue);
+    }
+
+}
