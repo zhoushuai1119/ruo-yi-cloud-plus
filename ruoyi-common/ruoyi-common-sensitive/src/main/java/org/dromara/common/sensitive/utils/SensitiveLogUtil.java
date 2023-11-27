b326a75a4b49592ed2a07adad79194536b7b3e86
zhoushuai <zs11195310@163.com>
2023-11-27 21:15:20 +0800

    脱敏日志util修改

--- ruoyi-common/ruoyi-common-sensitive/src/main/java/org/dromara/common/sensitive/utils/SensitiveLogUtil.java
+++ ruoyi-common/ruoyi-common-sensitive/src/main/java/org/dromara/common/sensitive/utils/SensitiveLogUtil.java
@@ -1,15 +1,11 @@
 package org.dromara.common.sensitive.utils;
 
 import cn.hutool.core.util.ArrayUtil;
-import cn.hutool.core.util.ObjectUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.dromara.common.core.utils.SpringUtils;
 import org.dromara.common.core.utils.reflect.ReflectUtils;
 import org.dromara.common.json.utils.JsonUtils;
 import org.dromara.common.sensitive.annotation.Sensitive;
-import org.dromara.common.sensitive.core.SensitiveService;
 import org.dromara.common.sensitive.core.SensitiveStrategy;
-import org.springframework.beans.BeansException;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -25,29 +21,6 @@ import java.util.Objects;
 public class SensitiveLogUtil {
 
     public static <T> String toJsonStr(T t) {
-        try {
-            SensitiveService sensitiveService = SpringUtils.getBean(SensitiveService.class);
-            if (ObjectUtil.isNotNull(sensitiveService) && sensitiveService.isSensitive()) {
-                return getSensitiveStr(t);
-            } else {
-                log.warn("脱敏设置为:false, 打印明文日志!!!");
-                return JsonUtils.toJsonString(t);
-            }
-        } catch (BeansException e) {
-            log.warn("脱敏实现不存在, 采用默认处理 => {}", e.getMessage());
-            return JsonUtils.toJsonString(t);
-        }
-    }
-
-    /**
-     * 获取脱敏字符串
-     *
-     * @author: zhou shuai
-     * @date: 2023/9/21 20:47
-     * @param: t
-     * @return: java.lang.String
-     */
-    private static <T> String getSensitiveStr(T t) {
         Field[] fields = ReflectUtils.getFields(t.getClass());
         if (ArrayUtil.isNotEmpty(fields)) {
             for (Field field : fields) {
