4c4d5f4a2d8ac1731c08a9840f415f3eb497d1f2
zhoushuai <zs11195310@163.com>
2024-02-09 21:23:08 +0800

    添加rocketmq-common模块

---
+++ ruoyi-common/ruoyi-common-rocketmq/src/main/java/com/alibaba/cloud/rocketmq/config/RocketMQConfiguration.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.rocketmq.config;
+
+import org.dromara.common.core.factory.YmlPropertySourceFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * RocketMQ 配置
+ *
+ * @author shuai.zhou
+ */
+@AutoConfiguration
+@PropertySource(value = "classpath:common-rocketmq.yml", factory = YmlPropertySourceFactory.class)
+public class RocketMQConfiguration {
+
+}
