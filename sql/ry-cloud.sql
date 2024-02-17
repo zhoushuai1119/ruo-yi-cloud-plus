d229b814425602c05450e270df2659232f369598
zhoushuai <zs11195310@163.com>
2024-02-17 15:52:27 +0800

    数据库链接修改

--- sql/ry-cloud.sql
+++ sql/ry-cloud.sql
@@ -1009,7 +1009,6 @@ CREATE TABLE IF NOT EXISTS undo_log
     UNIQUE KEY ux_undo_log (xid, branch_id)
 ) ENGINE = InnoDB COMMENT ='AT transaction mode undo table';
 
-
 -- ----------------------------
 -- 多数据源配置表
 -- ----------------------------
@@ -1028,3 +1027,26 @@ CREATE TABLE sys_datasource_config (
     `update_by`             bigint         DEFAULT NULL    COMMENT '更新者',
     primary key (`id`)
 ) ENGINE=InnoDB COMMENT='多数据源配置表';
+
+-- ----------------------------
+-- Sentinel监控信息表
+-- ----------------------------
+drop table if exists `metric`;
+CREATE TABLE `metric`
+(
+    `id`            bigint         NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `app`           varchar(255)   NULL DEFAULT NULL COMMENT '应用名称',
+    `resource`      varchar(255)   NULL DEFAULT NULL COMMENT '资源名称',
+    `timestamp`     datetime       NULL DEFAULT NULL COMMENT '监控信息时间戳',
+    `gmt_create`    datetime       NULL DEFAULT NULL COMMENT '创建时间',
+    `gmt_modified`  datetime       NULL DEFAULT NULL COMMENT '修改时间',
+    `pass_qps`      bigint         NULL DEFAULT NULL COMMENT '通过QPS',
+    `success_qps`   bigint         NULL DEFAULT NULL COMMENT '成功QPS',
+    `block_qps`     bigint         NULL DEFAULT NULL COMMENT '限流QPS',
+    `exception_qps` bigint         NULL DEFAULT NULL COMMENT '异常QPS',
+    `rt`            decimal(10, 2) NULL DEFAULT NULL COMMENT '资源的平均响应时间',
+    `count`         int            NULL DEFAULT NULL COMMENT '本次聚合的总条数',
+    `resource_code` int            NULL DEFAULT NULL COMMENT '资源hashcode',
+    PRIMARY KEY (`id`) USING BTREE,
+    INDEX `idx_app_timestamp` (`app` ASC, `timestamp` ASC) USING BTREE
+) ENGINE = InnoDB COMMENT = 'Sentinel监控信息表';
