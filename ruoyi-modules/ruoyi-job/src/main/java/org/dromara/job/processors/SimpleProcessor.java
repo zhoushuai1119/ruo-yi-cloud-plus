de603f0ca228e8aa8900d887e41164b33afa5586
zhoushuai <zs11195310@163.com>
2024-02-25 00:28:15 +0800

    移除powerjob-server-starter不必要的依赖

--- ruoyi-modules/ruoyi-job/src/main/java/org/dromara/job/processors/SimpleProcessor.java
+++ ruoyi-modules/ruoyi-job/src/main/java/org/dromara/job/processors/SimpleProcessor.java
@@ -1,8 +1,5 @@
 package org.dromara.job.processors;
 
-import cn.hutool.json.JSONUtil;
-import com.cloud.mq.base.core.CloudMQTemplate;
-import com.cloud.mq.base.dto.CloudMQSendResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import tech.powerjob.worker.core.processor.ProcessResult;
@@ -10,7 +7,6 @@ import tech.powerjob.worker.core.processor.TaskContext;
 import tech.powerjob.worker.core.processor.sdk.BasicProcessor;
 import tech.powerjob.worker.log.OmsLogger;
 
-import javax.annotation.Resource;
 import java.util.Optional;
 
 /**
@@ -21,11 +17,8 @@ import java.util.Optional;
 @Slf4j
 public class SimpleProcessor implements BasicProcessor {
 
-    @Resource
-    private CloudMQTemplate cloudMQTemplate;
-
     @Override
-    public ProcessResult process(TaskContext context) throws Exception {
+    public ProcessResult process(TaskContext context) {
 
         OmsLogger logger = context.getOmsLogger();
 
@@ -33,15 +26,10 @@ public class SimpleProcessor implements BasicProcessor {
         logger.info("Current context:{}", context.getWorkflowContext());
         logger.info("Current job params:{}", jobParams);
 
-        CloudMQSendResult cloudMQSendResult = cloudMQTemplate.syncSend(jobParams, "POWERJOB_WORKER", "SimpleProcessor test");
-        log.info("cloudMQSendResult is {}", JSONUtil.toJsonStr(cloudMQSendResult));
-
         // 测试中文问题 #581
         if (jobParams.contains("CN")) {
             return new ProcessResult(true, "任务成功啦！！！");
         }
-
         return jobParams.contains("F") ? new ProcessResult(false) : new ProcessResult(true, "yeah!");
-
     }
 }
