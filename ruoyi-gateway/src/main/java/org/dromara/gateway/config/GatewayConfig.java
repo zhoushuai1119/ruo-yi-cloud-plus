104d629b4e7d521d183bc4cef5d6012321080a6b
zhoushuai <zs11195310@163.com>
2024-02-27 00:21:53 +0800

    gateway sentinel自定义异常处理

--- ruoyi-gateway/src/main/java/org/dromara/gateway/config/GatewayConfig.java
+++ ruoyi-gateway/src/main/java/org/dromara/gateway/config/GatewayConfig.java
@@ -1,16 +1,35 @@
 package org.dromara.gateway.config;
 
+import cn.hutool.json.JSONUtil;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.authority.AuthorityException;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowException;
+import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowException;
+import com.alibaba.csp.sentinel.slots.system.SystemBlockException;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.common.core.domain.R;
 import org.dromara.gateway.handler.SentinelFallbackHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.dromara.common.core.constant.SentinelConstants.SENTINEL_GATEWAY_ERROR_MSG_HEADER;
+import static org.dromara.common.core.constant.SentinelConstants.SENTINEL_GATEWAY_HTTP_STATUS_CODE_HEADER;
 
 /**
  * 网关限流配置
  *
  * @author shuai.zhou
  */
+@Slf4j
 @Configuration
 public class GatewayConfig {
     @Bean
@@ -18,4 +37,37 @@ public class GatewayConfig {
     public SentinelFallbackHandler sentinelGatewayExceptionHandler() {
         return new SentinelFallbackHandler();
     }
+
+    /**
+     * 自定义限流异常
+     *
+     * @author: zhou shuai
+     * @date: 2024/2/27 0:09
+     */
+    @PostConstruct
+    public void initBlockHandlers() {
+        BlockRequestHandler blockRequestHandler = (serverWebExchange, throwable) -> {
+            String msg = "非法访问,请稍后重试";
+            HttpStatus httpStatus = HttpStatus.TOO_MANY_REQUESTS;
+            if (throwable instanceof FlowException || throwable instanceof ParamFlowException) {
+                msg = "您的访问过于频繁,请稍后重试!";
+            } else if (throwable instanceof DegradeException) {
+                msg = "调用服务响应异常,已进行降级!";
+            } else if (throwable instanceof SystemBlockException) {
+                msg = "已触碰系统的红线规则,请检查访问参数!";
+            } else if (throwable instanceof AuthorityException) {
+                httpStatus = HttpStatus.UNAUTHORIZED;
+                msg = "授权规则检测不同,请检查访问参数!";
+            }
+            R sentinelResp = R.fail(httpStatus.value(), msg);
+            log.info("Blocked by Sentinel Response : {}", JSONUtil.toJsonStr(sentinelResp));
+            return ServerResponse.status(httpStatus)
+                .header(SENTINEL_GATEWAY_HTTP_STATUS_CODE_HEADER, String.valueOf(httpStatus.value()))
+                .header(SENTINEL_GATEWAY_ERROR_MSG_HEADER, msg)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(sentinelResp));
+        };
+        GatewayCallbackManager.setBlockHandler(blockRequestHandler);
+    }
+
 }
