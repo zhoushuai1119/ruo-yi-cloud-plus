5923e592457ded9e1585aec8bf312b9ab6997112
zhoushuai <zs11195310@163.com>
2024-02-29 23:28:23 +0800

    添加monitor监控模块

---
+++ ruoyi-visual/ruoyi-monitor/src/main/java/org/dromara/monitor/RuoYiMonitorApplication.java
@@ -0,0 +1,18 @@
+package org.dromara.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 监控中心
+ *
+ * @author shuai.zhou
+ */
+@SpringBootApplication
+public class RuoYiMonitorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuoYiMonitorApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  监控中心启动成功   ლ(´ڡ`ლ)ﾞ  ");
+    }
+}
