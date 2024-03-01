41fa46d1ad3c6eec0c0a1eb3850ead00da0166e3
zhoushuai <zs11195310@163.com>
2024-03-01 21:40:54 +0800

    添加rocketmq-monitor监控模块

--- ruoyi-visual/ruoyi-monitor/ruoyi-rocketmq-monitor/src/main/java/org/dromara/monitor/rocketmq/RocketMQMonitorApplication.java
+++ ruoyi-visual/ruoyi-monitor/ruoyi-rocketmq-monitor/src/main/java/org/dromara/monitor/rocketmq/RocketMQMonitorApplication.java
@@ -2,21 +2,18 @@ package org.dromara.monitor.rocketmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 监控中心
  *
  * @author shuai.zhou
  */
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class RocketMQMonitorApplication {
 
-    public static final String ROCKETMQ_CLIENT_LOG_LOADCONFIG = "rocketmq.client.log.loadconfig";
-
     public static void main(String[] args) {
-        System.setProperty(ROCKETMQ_CLIENT_LOG_LOADCONFIG, "false");
-        SpringApplication.run(RuoYiMQMonitorApplication.class, args);
+        System.setProperty("rocketmq.client.log.loadconfig", "false");
+        SpringApplication.run(RocketMQMonitorApplication.class, args);
         System.out.println("(♥◠‿◠)ﾉﾞ  RocketMQ监控中心启动成功   ლ(´ڡ`ლ)ﾞ  ");
     }
 }
