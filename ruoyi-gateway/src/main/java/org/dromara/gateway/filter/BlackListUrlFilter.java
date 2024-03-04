6b370ff93921305830e0d130263ec66cfc0cd62c
zhoushuai <zs11195310@163.com>
2024-03-04 20:29:38 +0800

    关闭api加密配置

--- ruoyi-gateway/src/main/java/org/dromara/gateway/filter/BlackListUrlFilter.java
+++ ruoyi-gateway/src/main/java/org/dromara/gateway/filter/BlackListUrlFilter.java
@@ -1,5 +1,6 @@
 package org.dromara.gateway.filter;
 
+import cn.hutool.core.collection.CollUtil;
 import org.dromara.gateway.utils.WebFluxUtils;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -34,24 +35,10 @@ public class BlackListUrlFilter extends AbstractGatewayFilterFactory<BlackListUr
     }
 
     public static class Config {
-        private List<String> blacklistUrl;
-
-        private List<Pattern> blacklistUrlPattern = new ArrayList<>();
+        private final List<Pattern> blacklistUrlPattern = new ArrayList<>();
 
         public boolean matchBlacklist(String url) {
-            return !blacklistUrlPattern.isEmpty() && blacklistUrlPattern.stream().anyMatch(p -> p.matcher(url).find());
-        }
-
-        public List<String> getBlacklistUrl() {
-            return blacklistUrl;
-        }
-
-        public void setBlacklistUrl(List<String> blacklistUrl) {
-            this.blacklistUrl = blacklistUrl;
-            this.blacklistUrlPattern.clear();
-            this.blacklistUrl.forEach(url -> {
-                this.blacklistUrlPattern.add(Pattern.compile(url.replaceAll("\\*\\*", "(.*?)"), Pattern.CASE_INSENSITIVE));
-            });
+            return CollUtil.isNotEmpty(blacklistUrlPattern) && blacklistUrlPattern.stream().anyMatch(p -> p.matcher(url).find());
         }
     }
 
