ea42ba2131596aa3f1302a7825d6468e22574851
zhoushuai <zs11195310@163.com>
2024-03-04 20:08:20 +0800

    关闭api加密配置

--- ruoyi-gateway/src/main/java/org/dromara/gateway/filter/AuthFilter.java
+++ ruoyi-gateway/src/main/java/org/dromara/gateway/filter/AuthFilter.java
@@ -30,7 +30,7 @@ public class AuthFilter {
         return new SaReactorFilter()
             // 拦截地址
             .addInclude("/**")
-            .addExclude("/favicon.ico", "/actuator/**", "/webjars/**", "/demo/**")
+            .addExclude("/favicon.ico", "/actuator/**", "/webjars/**")
             // 鉴权方法：每次访问进入
             .setAuth(obj -> {
                 // 登录校验 -- 拦截所有路由
