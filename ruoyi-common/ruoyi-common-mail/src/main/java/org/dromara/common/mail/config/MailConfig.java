182250b15e6a41940a3dcfe289b49f6c8d0b940a
zhoushuai <zs11195310@163.com>
2024-04-12 19:33:11 +0800

    common-sms & common-mail配置抽离

--- ruoyi-common/ruoyi-common-mail/src/main/java/org/dromara/common/mail/config/MailConfig.java
+++ ruoyi-common/ruoyi-common-mail/src/main/java/org/dromara/common/mail/config/MailConfig.java
@@ -1,11 +1,13 @@
 package org.dromara.common.mail.config;
 
+import org.dromara.common.core.factory.YmlPropertySourceFactory;
 import org.dromara.common.mail.config.properties.MailProperties;
 import org.dromara.common.mail.utils.MailAccount;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * JavaMail 配置
@@ -14,6 +16,7 @@ import org.springframework.context.annotation.Bean;
  */
 @AutoConfiguration
 @EnableConfigurationProperties(MailProperties.class)
+@PropertySource(value = "classpath:common-mail.yml", factory = YmlPropertySourceFactory.class)
 public class MailConfig {
 
     @Bean
--- ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsAutoConfiguration.java
+++ ruoyi-common/ruoyi-common-sms/src/main/java/org/dromara/common/sms/config/SmsAutoConfiguration.java
@@ -1,11 +1,13 @@
 package org.dromara.common.sms.config;
 
+import org.dromara.common.core.factory.YmlPropertySourceFactory;
 import org.dromara.common.redisson.config.RedissonConfiguration;
 import org.dromara.common.sms.core.dao.PlusSmsDao;
 import org.dromara.sms4j.api.dao.SmsDao;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 
 /**
  * 短信配置类
@@ -13,6 +15,7 @@ import org.springframework.context.annotation.Primary;
  * @author shuai.zhou
  */
 @AutoConfiguration(after = {RedissonConfiguration.class})
+@PropertySource(value = "classpath:common-sms.yml", factory = YmlPropertySourceFactory.class)
 public class SmsAutoConfiguration {
 
     @Primary
