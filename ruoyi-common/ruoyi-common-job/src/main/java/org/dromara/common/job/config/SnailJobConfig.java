645deb9de34d3727f0881db714af05e31a91bae5
zhoushuai <zs11195310@163.com>
2024-05-19 14:23:05 +0800

    EasyRetry-v3.2.0 => SnailJob-v1.0.0-beta1

--- ruoyi-common/ruoyi-common-job/src/main/java/org/dromara/common/job/config/SnailJobConfig.java
+++ ruoyi-common/ruoyi-common-job/src/main/java/org/dromara/common/job/config/SnailJobConfig.java
@@ -4,13 +4,13 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import cn.hutool.core.collection.CollUtil;
-import com.aizuda.easy.retry.client.common.appender.EasyRetryLogbackAppender;
-import com.aizuda.easy.retry.client.common.event.ChannelReconnectEvent;
-import com.aizuda.easy.retry.client.common.event.EasyRetryStartingEvent;
-import com.aizuda.easy.retry.client.starter.EnableEasyRetry;
+import com.aizuda.snailjob.client.common.appender.SnailLogbackAppender;
+import com.aizuda.snailjob.client.common.event.SnailChannelReconnectEvent;
+import com.aizuda.snailjob.client.common.event.SnailClientStartingEvent;
+import com.aizuda.snailjob.client.starter.EnableSnailJob;
 import jakarta.annotation.Resource;
 import org.dromara.common.core.utils.StringUtils;
-import org.dromara.common.job.config.properties.EasyRetryServerProperties;
+import org.dromara.common.job.config.properties.SnailJobServerProperties;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -25,41 +25,41 @@ import java.util.List;
 /**
  * 启动定时任务
  *
- * @author dhb52
+ * @author shuai.zhou
  * @since 2024/3/12
  */
 @AutoConfiguration
-@EnableConfigurationProperties(EasyRetryServerProperties.class)
-@ConditionalOnProperty(prefix = "easy-retry", name = "enabled", havingValue = "true")
+@EnableConfigurationProperties(SnailJobServerProperties.class)
+@ConditionalOnProperty(prefix = "snail-job", name = "enabled", havingValue = "true")
 @EnableScheduling
-@EnableEasyRetry(group = "${easy-retry.group-name}")
-public class EasyRetryConfig {
+@EnableSnailJob(group = "${snail-job.group-name}")
+public class SnailJobConfig {
 
     /**
      * 服务端host
      */
-    private static final String EASY_RETRY_SERVER_HOST = "easy-retry.server.host";
+    private static final String SNAIL_JOB_SERVER_HOST = "snail-job.server.host";
     /**
      * 服务端端口
      */
-    private static final String EASY_RETRY_SERVER_PORT = "easy-retry.server.port";
+    private static final String SNAIL_JOB_SERVER_PORT = "snail-job.server.port";
 
     @Resource
-    private EasyRetryServerProperties properties;
+    private SnailJobServerProperties properties;
 
     @Resource
     private DiscoveryClient discoveryClient;
 
-    @EventListener(EasyRetryStartingEvent.class)
-    public void onStarting(EasyRetryStartingEvent event) {
+    @EventListener(SnailClientStartingEvent.class)
+    public void onStarting(SnailClientStartingEvent event) {
         // 从 nacos 获取 server 服务连接
         registerServer();
         // 注册 日志监控配置
         registerLogging();
     }
 
-    @EventListener(ChannelReconnectEvent.class)
-    public void onReconnect(ChannelReconnectEvent event) {
+    @EventListener(SnailChannelReconnectEvent.class)
+    public void onReconnect(SnailChannelReconnectEvent event) {
         // 连接中断 重新从 nacos 获取存活的服务连接(高可用配置)
         registerServer();
     }
@@ -70,16 +70,16 @@ public class EasyRetryConfig {
             List<ServiceInstance> instances = discoveryClient.getInstances(serverName);
             if (CollUtil.isNotEmpty(instances)) {
                 ServiceInstance instance = instances.get(0);
-                System.setProperty(EASY_RETRY_SERVER_HOST, instance.getHost());
-                System.setProperty(EASY_RETRY_SERVER_PORT, properties.getPort());
+                System.setProperty(SNAIL_JOB_SERVER_HOST, instance.getHost());
+                System.setProperty(SNAIL_JOB_SERVER_PORT, properties.getPort());
             }
         }
     }
 
     private void registerLogging() {
         LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        EasyRetryLogbackAppender<ILoggingEvent> ca = new EasyRetryLogbackAppender<>();
-        ca.setName("easy_log_appender");
+        SnailLogbackAppender<ILoggingEvent> ca = new SnailLogbackAppender<>();
+        ca.setName("snail_log_appender");
         ca.start();
         Logger rootLogger = lc.getLogger(Logger.ROOT_LOGGER_NAME);
         rootLogger.addAppender(ca);
